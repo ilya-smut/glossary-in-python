0849bdac71d0ac62b96839a5edf00b9293d91a71
Ilya Smut <107916229+ilya-smut@users.noreply.github.com>
2022-11-12 14:12:24 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from classes import Glossary
+
+glossary = Glossary()
+
+while True:
+    word = input('Please, enter the word: ')
+    with open('output.txt', 'w') as f:
+        f.write(glossary.get_and_build(word))
+    glossary.get_and_print(word)
+
